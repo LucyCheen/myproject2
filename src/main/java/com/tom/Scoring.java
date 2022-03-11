99cdefca728832814dd824f1a52c3c302fc9a52b
User <ac020862@gmail.com>
2022-03-11 15:29:23 +0800

    inital commit

--- src/main/java/com/tom/Scoring.java
+++ src/main/java/com/tom/Scoring.java
@@ -2,9 +2,10 @@ package com.tom;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student Cody = new Student();
-        Cody.name = "Karen";
-        Cody.englishs =80;
-        Cody.maths=45;
+        Student Cody = new Student("Cody",80,45);
+        Student Toy = new Student("Toy",70,20);
+        Student s = new Student();
+        Cody.print();
+        Toy.print();
     }
 }
--- src/main/java/com/tom/Student.java
+++ src/main/java/com/tom/Student.java
@@ -1,8 +1,32 @@
 package com.tom;
 
+import java.sql.SQLOutput;
+
 public class Student {
     String name;
     int englishs;
     int maths;
-
+    public Student(String name){
+        this.name = name;
+    }
+    public Student(String name,int englishs,int maths){
+    //this.name = name;
+    this.englishs = englishs;
+    this.maths = maths;
+    }
+    public Student (){
+        this ("Jone Doe",-1,-1);
+        /*this("John Doe",-1,-1)
+        name = "John Doe";
+        englishs = -1
+        maths = -1;*/
+    }
+    public void print(){
+        int average = ((englishs+maths)/2);
+        System.out.print(name + "\t" + englishs+"\t" + maths + "\t" + average);
+        if (average < 60){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
